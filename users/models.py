dbe9668bb91cd49afa416612ebca47ef89fb4a9c
Oleg Timershayakhov <olegtimer@yandex.ru>
2025-07-26 17:21:37 +0500

    add model user

--- users/models.py
+++ users/models.py
@@ -2,4 +2,44 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-	pass
+
+	username = models.CharField(
+		max_length=50,
+		unique=True,
+		verbose_name="Логин пользователя",
+		help_text="Укажите логин пользователя",
+	)
+
+	phone_number = models.CharField(
+		max_length=20,
+		unique=True,
+		verbose_name="Номер телефона",
+		help_text="Укажите номер телефона",
+		null=True,
+		blank=True
+	)
+
+	birth_date = models.DateField(
+		verbose_name="Дата рождения",
+		help_text="Укажите дату рождения",
+		null=True,
+		blank=True
+	)
+
+	created_at = models.DateTimeField(
+		auto_now_add=True,
+		verbose_name="Дата создания"
+	)
+
+	updated_at = models.DateTimeField(
+		auto_now=True,
+		verbose_name="Дата редактирования"
+	)
+
+
+	class Meta:
+		verbose_name = "Пользователь"
+		verbose_name_plural = "Пользователи"
+
+	def __str__(self):
+		return self.username
