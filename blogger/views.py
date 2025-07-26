9d592c721f2ddc8c548ffffdff81cc797fa96f09
Oleg Timershayakhov <olegtimer@yandex.ru>
2025-07-26 18:28:22 +0500

    add PostViewSet

--- blogger/views.py
+++ blogger/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from blogger.models import Post
+from blogger.serializers import PostSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
