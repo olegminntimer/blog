dbb335989aeffe6f205cb2a546b7b1a787e7501d
Oleg Timershayakhov <olegtimer@yandex.ru>
2025-07-27 09:37:46 +0500

    + posts_router.urls

--- blogger/urls.py
+++ blogger/urls.py
@@ -7,12 +7,14 @@ from blogger.views import PostViewSet, CommentViewSet
 
 app_name = BloggerConfig.name
 
+# Базовый роутер
 router = DefaultRouter()
 router.register(r"posts", PostViewSet, basename="posts")
 
+# Вложенный роутер для комментариев к постам
 posts_router = routers.NestedSimpleRouter(router, r'posts', lookup='post')
 posts_router.register(r'comments', CommentViewSet, basename='post-comments')
 
 urlpatterns = [
 
-] + router.urls
+] + router.urls + posts_router.urls
